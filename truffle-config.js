82966feb3ef33ea60fbdb4f2333c25b4841c219f
Anthony Albertorio <anthony.albertorio@gmail.com>
2022-06-21 18:05:25 -0400

    update truffle-config to rinkebydeployment.

--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@
 
 require("dotenv").config(); // allows usage of .env file to store secrets
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const infuraURL = `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+const infuraURL = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
 const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
@@ -57,10 +57,10 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, infuraURL),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      network_id: 4, // Rinkeby's id
+      gas: 5500000, // Rinkeby has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
@@ -81,7 +81,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.11", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.13", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
