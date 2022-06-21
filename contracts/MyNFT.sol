88573b79ace567446fc94bb5a313a78c62d567e2
Anthony Albertorio <anthony.albertorio@gmail.com>
2022-06-21 18:05:48 -0400

    updated MyNFT.sol contract to simpler deployment.

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,35 +1,28 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/4.x/erc721](https://docs.openzeppelin.com/contracts/4.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721 {
-  using Counters for Counters.Counter;
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  Counters.Counter private currentTokenId;
+    constructor() ERC721("MyNFT", "NFT") {}
 
-  /// @dev Base token URI used as a prefix by tokenURI().
-  string public baseTokenURI;
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
 
-  constructor() ERC721("NFTTutorial", "NFT") {
-    baseTokenURI = "";
-  }
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-  function mintTo(address recipient) public returns (uint256) {
-    currentTokenId.increment();
-    uint256 newItemId = currentTokenId.current();
-    _safeMint(recipient, newItemId);
-    return newItemId;
-  }
-
-  /// @dev Returns an URI for a given token ID
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseTokenURI;
-  }
-
-  /// @dev Sets the base token URI prefix.
-  function setBaseTokenURI(string memory _baseTokenURI) public {
-    baseTokenURI = _baseTokenURI;
-  }
+        return newItemId;
+    }
 }
