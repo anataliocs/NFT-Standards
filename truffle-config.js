dfdb6ec9335300ace2180a89fc3eea9bf47206f6
Anthony Albertorio <anthony.albertorio@gmail.com>
2022-06-21 12:36:15 -0400

    update to truffle-config.js for ropsten and development network

--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@
 
 require("dotenv").config(); // allows usage of .env file to store secrets
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const infuraURL = "https://ropsten.infura.io/v3/" + INFURA_PROJECT_ID;
+const infuraURL = `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
 const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
@@ -41,11 +41,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
