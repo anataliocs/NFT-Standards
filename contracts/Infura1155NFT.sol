8276cbe2a373d4e67f25ab7320a4a566aef84be6
chris anatalio <chris.anatalio@consensys.net>
2022-07-13 19:56:49 -0700

    Update metadata and images

--- contracts/Infura1155NFT.sol
+++ contracts/Infura1155NFT.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Infura1155NFT is ERC1155, ERC1155Metadata {
+contract Infura1155NFT is ERC1155, Ownable {
     uint256 public constant GOLD = 0;
     uint256 public constant RARE_ITEM = 1;
     uint256 public constant EPIC_ITEM = 2;
@@ -12,7 +13,7 @@ contract Infura1155NFT is ERC1155, ERC1155Metadata {
 
     function mint(address recipient) public returns (uint256)
     {
-        _mint(recipient, GOLD, 100, "");
+        _mint(recipient, GOLD, 10, "");
         _mint(recipient, RARE_ITEM, 1, "");
         _mint(recipient, EPIC_ITEM, 1, "");
 
