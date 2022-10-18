bd119ff1fa28c047e280c604c26cff1ad41fe2e9
chris anatalio <chris.anatalio@consensys.net>
2022-10-18 20:12:04 +0200

    Update for polygon mumbai deployment target

--- truffle-config.js
+++ truffle-config.js
@@ -21,6 +21,7 @@
 require("dotenv").config(); // allows usage of .env file to store secrets
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const infuraURL = `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_SECRET}`;
+const infuraPolygonURL = `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_SECRET}`;
 const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
@@ -60,6 +61,12 @@ module.exports = {
     goerli: {
       provider: () => new HDWalletProvider(mnemonic, infuraURL),
       network_id: 5, // Goerli's id
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider(mnemonic, infuraPolygonURL),
+      network_id: 80001, // mumbai's id
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
