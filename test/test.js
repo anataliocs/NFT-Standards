d9709b17bf108ebd84c04e1bdc855cb2c0911585
Chris Anatalio <chris.anatalio@consensys.net>
2023-01-09 13:13:48 -0800

    Add mint tests (#3)
    * Add mint tests
    
    Fix test
    
    Signed-off-by: chris anatalio <chris.anatalio@consensys.net>
    
    * Code cleanup and update readme
    
    * Bump @openzeppelin/contracts from 4.6.0 to 4.7.3 (#4)
    
    Bumps [@openzeppelin/contracts](https://github.com/OpenZeppelin/openzeppelin-contracts) from 4.6.0 to 4.7.3.
    - [Release notes](https://github.com/OpenZeppelin/openzeppelin-contracts/releases)
    - [Changelog](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/OpenZeppelin/openzeppelin-contracts/compare/v4.6.0...v4.7.3)
    
    ---
    updated-dependencies:
    - dependency-name: "@openzeppelin/contracts"
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Fixes from Emily
    
    Signed-off-by: chris anatalio <chris.anatalio@consensys.net>
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/test.js
+++ test/test.js
@@ -9,11 +9,24 @@ let infura1155NFTInstance;
 contract('Infura721aNFTContract', function (accounts) {
 
     it("Contract deployment", function () {
-        //Fetching the contract instance of our smart contract
         return Infura721aNFT.deployed().then(function (instance) {
             infura721aNFTInstance = instance;
             assert(infura721aNFTInstance !== undefined, 'Infura721aNFT contract should be defined');
         });
+    });
+
+    it("Should mint Infura721aNFT token", function () {
+        const toAddress = accounts[1];
+        return infura721aNFTInstance.mint(toAddress, 1, {
+            from: accounts[0]
+        })
+            .then(function (result) {
+
+                assert(result.logs[0].event === 'Transfer', 'Transfer Event should have been emitted');
+                assert(result.logs[0].args[0] === '0x0000000000000000000000000000000000000000', 'Transfer Event arg[0] should be sender');
+                assert(result.logs[0].args[1] === toAddress, 'Transfer Event arg[1] should be receiver');
+                assert(result.logs[0].args[2].toNumber() === 0, 'Transfer Event arg[2] should be id');
+            })
     });
 
 });
@@ -21,11 +34,24 @@ contract('Infura721aNFTContract', function (accounts) {
 contract('Infura721NFTContract', function (accounts) {
 
     it("Contract deployment", function () {
-        //Fetching the contract instance of our smart contract
         return Infura721NFT.deployed().then(function (instance) {
             infura721NFTInstance = instance;
             assert(infura721NFTInstance !== undefined, 'Infura721NFT contract should be defined');
         });
+    });
+
+    it("Should mint Infura721NFT token", function () {
+        const toAddress = accounts[1];
+        return infura721NFTInstance.mintNFT(toAddress, {
+            from: accounts[0]
+        })
+            .then(function (result) {
+
+                assert(result.logs[0].event === 'Transfer', 'Transfer Event should have been emitted');
+                assert(result.logs[0].args[0] === '0x0000000000000000000000000000000000000000', 'Transfer Event arg[0] should be sender');
+                assert(result.logs[0].args[1] === toAddress, 'Transfer Event arg[1] should be receiver');
+                assert(result.logs[0].args[2].toNumber() === 1, 'Transfer Event arg[2] should be id');
+            })
     });
 
 });
@@ -33,11 +59,26 @@ contract('Infura721NFTContract', function (accounts) {
 contract('Infura1155NFTContract', function (accounts) {
 
     it("Contract deployment", function () {
-        //Fetching the contract instance of our smart contract
         return Infura1155NFT.deployed().then(function (instance) {
             infura1155NFTInstance = instance;
             assert(infura1155NFTInstance !== undefined, 'Infura1155NFT contract should be defined');
         });
+    });
+
+    it("Should mint Infura1155NFT token", async function () {
+        const toAddress = accounts[1];
+        return infura1155NFTInstance.mint(toAddress, {
+            from: accounts[0]
+        })
+            .then(function (result) {
+                assert(result.logs.length === 3, '3 TransferSingle Events should have been emitted');
+                assert(result.logs[0].event === 'TransferSingle', 'Transfer Event should have been emitted');
+                assert(result.logs[0].args[0] === '0xC7E08ecBdbA5Fbf8b9F7d076Ad01e0f58C5B2647', 'Transfer Event arg[0] should be operator');
+                assert(result.logs[0].args[1] === '0x0000000000000000000000000000000000000000', 'Transfer Event arg[1] should be sender');
+                assert(result.logs[0].args[2] === toAddress, 'Transfer Event arg[1] should be receiver');
+                assert(result.logs[0].args[3].toNumber() === 0, 'Transfer Event arg[3] should be id');
+                assert(result.logs[0].args[4].toNumber() === 10, 'Transfer Event arg[4] should be value');
+            })
     });
 
 });
